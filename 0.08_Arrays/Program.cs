16ae8f1e6efb44fe92d017bb23d296cf12e3c962
Gavin Williams <gjosephw24@gmail.com>
2017-10-26 15:50:40 -0400

    Added Arrays and Dictionaries

---
+++ 0.08_Arrays/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._08_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BRONZE
+            string[] favFood = new string[3];
+            favFood[0] = "Bacon";
+            favFood[1] = "Biscuits and Gravy";
+            favFood[2] = "Pizza";
+
+            foreach (string fav in favFood)
+            {
+                Console.WriteLine(fav);
+            }
+
+            //SILVER
+            int[] numbers1 = new int[5] { 3, 4, 4, 5, 5 };
+            int[] numbers2 = new int[5] { 7, 2, 43, 2, 12 };
+
+            int[] numbers3 = new int[5];
+
+            for (int i = 0; i < numbers3.Length; i++)
+            {
+                numbers3[i] = numbers1[i] + numbers2[i];
+            }
+
+            foreach (int number in numbers3)
+            {
+                Console.WriteLine(number);
+            }
+
+            //GOLD
+            int[] randomNumbers = new int[10];
+            Random rnd = new Random();
+
+            for (int i = 0; i < randomNumbers.Length; i++)
+            {
+                randomNumbers[i] = rnd.Next();
+            }
+
+            foreach (int number in randomNumbers)
+            {
+                Console.WriteLine(number);
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 0.08_CollectionsDictionaries/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._08_CollectionsDictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> award = new Dictionary<string, string>()
+            {
+                { "2000", "Gladiator" },
+                { "2001", "A Beautiful Mind" },
+                { "2002", "Chicago"},
+                { "2003", "The Lord of the Rings: Return of the King"},
+                { "2004", "Million Dollar Baby"},
+                { "2005", "Crash"}
+            };
+
+            foreach (var movie in award)
+            {
+                Console.WriteLine($"In {movie.Key} the winner was {movie.Value}");
+            }
+
+
+
+            var nominees = new Dictionary<string, string[]>()
+            {
+                {"2000", new string[]{"Gladiator", "Crouching Tiger: Hidden Dragon", "Traffic"} },
+                {"2001", new string[]{"A Beautiful Mind", "The Lord of the Rings: The Fellowship of the Ring", "Moulin Rouge!"} },
+                {"2002", new string[]{"Chicago", "The Pianist", "The Lord of the Rings: The Two Towers"} },
+
+            };
+
+            foreach (KeyValuePair<string, string[]> pair in nominees)
+            {
+                Console.WriteLine("{0}: {1}, {2}", pair.Key, pair.Value[0], pair.Value[1]);
+            }
+
+
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
