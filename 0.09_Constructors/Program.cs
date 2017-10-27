04b6a38ae26f0fd74b1f07c7202f5b83fbe5d0f9
Gavin Williams <gjosephw24@gmail.com>
2017-10-27 10:11:09 -0400

    Added constructors

---
+++ 0.09_Constructors/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._09_Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student paulStudent = new Student();
+            paulStudent.FirstName = "Paul";
+            paulStudent.LastName = "OConnor";
+            paulStudent.Age = 41;
+            paulStudent.Grade = 12;
+
+            Student alex = new Student()
+
+
+            Student jayStudent = new Student("Gavin", "Williams", 20, 1);
+            
+
+
+        }
+    }
+}
