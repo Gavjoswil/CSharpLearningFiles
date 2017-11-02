c1d1f5900bd04619aed9f9a1ae10e976d5659183
Gavin Williams <gjosephw24@gmail.com>
2017-11-02 10:35:38 -0400

    Updated quiz

--- 0.14_Quiz1/Program.cs
+++ 0.14_Quiz1/Program.cs
@@ -13,14 +13,33 @@ namespace _0._14_Quiz1
             Triangle triangle1 = new Triangle(5, 6);
             Triangle triangle2 = new Triangle(10, 15);
             Triangle triangle3 = new Triangle(10, 5);
-            Triangle triangle4 = new Triangle(8, 6);
+            Triangle triangle4 = new Triangle(9, 3);
             Triangle triangle5 = new Triangle(113, 90);
 
-            Console.WriteLine(triangle1.Area());
-            Console.WriteLine(triangle2.Area());
-            Console.WriteLine(triangle3.Area());
-            Console.WriteLine(triangle4.Area());
-            Console.WriteLine(triangle5.Area());
+            triangle1.Area();
+            triangle2.Area();
+            triangle3.Area();
+            triangle4.Area();
+            triangle5.Area();
+
+
+            IEnumerable<Triangle> triangles = new List<Triangle>
+            {
+                triangle1,
+                triangle2,
+                triangle3,
+                triangle4,
+                triangle5
+            };
+            IEnumerable<Triangle> query = from t in triangles
+                                             where t.TArea > 20
+                                             orderby t.TArea ascending
+                                             select t;
+            foreach (Triangle triangle in query)
+            {
+                Console.WriteLine(triangle.TArea);
+            }
+
 
             Console.ReadLine();
         }
--- 0.14_Quiz1/Triangle.cs
+++ 0.14_Quiz1/Triangle.cs
@@ -8,17 +8,25 @@ namespace _0._14_Quiz1
 {
     class Triangle : Shape
     {
-        public Triangle(int tBase, int tHeight)
+        public Triangle(int tbase, int theight)
         {
-            this.TBase = tBase;
-            this.THeight = tHeight;
+            this.TBase = tbase;
+            this.THeight = theight;
         }
         public int TBase { get; set; }
         public int THeight { get; set; }
+        public double TArea { get; set; }
 
+
+        //What Jay Did
         public override double Area()
         {
-            return (.5) * (TBase) * (THeight);
+            TArea = this.TBase * this.THeight / 2.0;
+            return TArea;
         }
+        //public override double Area()
+        //{
+        //    return (.5) * (TBase) * (THeight);
+        //}
     }
 }
