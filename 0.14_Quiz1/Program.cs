6350fede5af23c0089a0b0246816ff2d386caddc
Gavin Williams <gjosephw24@gmail.com>
2017-11-02 09:33:53 -0400

    Added QuizOne

---
+++ 0.14_Quiz1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._14_Quiz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle1 = new Triangle(5, 6);
+            Triangle triangle2 = new Triangle(10, 15);
+            Triangle triangle3 = new Triangle(10, 5);
+            Triangle triangle4 = new Triangle(8, 6);
+            Triangle triangle5 = new Triangle(113, 90);
+
+            Console.WriteLine(triangle1.Area());
+            Console.WriteLine(triangle2.Area());
+            Console.WriteLine(triangle3.Area());
+            Console.WriteLine(triangle4.Area());
+            Console.WriteLine(triangle5.Area());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 0.14_Quiz1/Triangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._14_Quiz1
+{
+    class Triangle : Shape
+    {
+        public Triangle(int tBase, int tHeight)
+        {
+            this.TBase = tBase;
+            this.THeight = tHeight;
+        }
+        public int TBase { get; set; }
+        public int THeight { get; set; }
+
+        public override double Area()
+        {
+            return (.5) * (TBase) * (THeight);
+        }
+    }
+}
